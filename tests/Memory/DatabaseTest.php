1dac23e9cc8db90a393c2774943a38d7da8425b1
Mehdi Aroui <mehdipsy3@gmail.com>
2019-09-05 13:41:32 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- tests/Memory/DatabaseTest.php
+++ tests/Memory/DatabaseTest.php
@@ -7,16 +7,15 @@ use Blogify\Tests\BlogifyTestBase;
 
 class DatabaseTest extends BlogifyTestBase
 {
-
-	public function test_first()
-	{
-		$this->assertTrue(true);
-	}
+    public function test_first()
+    {
+        $this->assertTrue(true);
+    }
 
     public function test_post_assertion()
     {
         factory(Post::class)->create();
 
         $this->assertCount(1, Post::all());
-	}
+    }
 }
